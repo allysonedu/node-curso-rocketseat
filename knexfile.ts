3c027fe2d7e5304f6ba161d3f77929d0bc00c83f
allysonedu <allysoneduu157@gmail.com>
2025-02-01 16:25:56 -0300

    criação de migrations

---
+++ knexfile.ts
@@ -0,0 +1,3 @@
+import { config } from "./src/config/configFile";
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
 import fastify from "fastify";
-import { knex } from "./database";
 
 const app = fastify();
-
-app.get("/hello", async (req, reply) => {
-  const tables = await knex("sqlite_schema").select("*");
-
-  return { message: "Hello, World!", tables };
-});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
